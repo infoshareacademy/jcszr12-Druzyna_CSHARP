d4090be1e7c232d8a8bc4b4c22752fe3f057a23b
UndergroundRobotics <robert.nizio@gmail>
2024-01-18 18:10:31 +0100

    Added class EntryTimeServices

---
+++ Solution1/ProjectClock.BussinesLogic/Services/EntryTimeServices.cs
@@ -0,0 +1,50 @@
+﻿using ProjectClock.BusinessLogic.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Services
+{
+    public class EntryTimeServices :ProjectServices
+    {
+        public static TimeEnter Start(int idOfProject)
+        {
+            var timeEnter = new TimeEnter();
+            timeEnter.StartTime = DateTime.Now;
+            timeEnter.Project = GetProject(idOfProject);
+
+            //Console.WriteLine($"\n Selected project ID: {project.Id} ");
+            //Console.WriteLine($"\n Selected project name: {project.Name} ");
+            //Console.WriteLine($"\n Start time of working on the project {DateTime.Now} ");
+
+            return timeEnter;
+        }
+
+        public static TimeEnter Stop(string descriptionActivitysInProject, TimeEnter timeEnter)
+        { 
+            timeEnter.EndTime = DateTime.Now;
+            timeEnter.Description = descriptionActivitysInProject;
+
+            //Console.WriteLine($"\n End of working time on the project: {timeEnter.EndTime} ");
+            //Console.WriteLine($"\n Description activities in project : {timeEnter.Description} ");
+
+            return timeEnter;
+        }
+
+        public static TimeEnter CalculateTime(TimeEnter timeEnter)
+        {
+            var project = new Project();
+            project.TotalProjectTime = timeEnter.EndTime - timeEnter.StartTime;
+            timeEnter.Project = project;
+
+            //Console.WriteLine($"\n Working time on the project: {timeEnter.Project.TotalProjectTime} ");
+            
+            return timeEnter; 
+        }
+    }
+}
