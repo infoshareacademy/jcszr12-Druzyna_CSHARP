a09cc1000137ff40a1efe7991ed716adca6251d1
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-01-20 17:42:26 +0100

    bug fixed

--- Solution1/ProjectClock.UI/Program.cs
+++ Solution1/ProjectClock.UI/Program.cs
@@ -5,13 +5,13 @@ using ProjectClock.UI.Menu;
 
 
 namespace ProjectClock.UI
-{
+{   
     internal class Program
     {
         static void Main(string[] args)
         {
-           MainMenu mainMenu = new MainMenu();
-           mainMenu.RunMainMenu();
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.RunMainMenu();
         }         
 
     }
