5d4ed7d7e8a3063460565d6f706515a5af996df1
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-01-22 19:59:16 +0100

    Fixed method to remove projects from list. Add sorting by id in GetProjectList

--- Solution1/ProjectClock.BussinesLogic/Services/ProjectServices/ProjectEditor.cs
+++ Solution1/ProjectClock.BussinesLogic/Services/ProjectServices/ProjectEditor.cs
@@ -1,5 +1,7 @@
-﻿using ProjectClock.BusinessLogic.Models;
+﻿using Newtonsoft.Json.Linq;
+using ProjectClock.BusinessLogic.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +13,18 @@ namespace ProjectClock.BusinessLogic.Services.ProjectServices
     {
         public static bool ModifyProject(int id,int idNew, string nameNew)
         {
-            var projects = ProjectGetter.GetProjectList();         
-            var project = GetProject(id);
-            if(project == null )
-            {
-                return false;
-            }
+            var projects = ProjectGetter.GetProjectList();          
             var path = GetDirectoryToFileFromDataFolder("projects.json");
 
-            projects.Remove(project);
-            SetID(project, idNew);
-            SetName(project, nameNew);
-            projects.Add(project);
-
-            ProjectSaver.Save(projects, path);
-            return true;
+            var project = projects.FirstOrDefault(x => x.Id == id);
+            if (project != null)
+            {
+                SetID(project, idNew);
+                SetName(project, nameNew);
+                ProjectSaver.Save(projects, path);
+                return true;
+            }
+            return false;
         }
-
-
     }
 }
