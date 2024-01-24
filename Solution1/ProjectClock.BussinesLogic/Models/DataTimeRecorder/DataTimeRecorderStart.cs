852f8a8e96110d3a14b533f21d8e44b5d25d5bb5
UndergroundRobotics <robert.nizio@gmail>
2024-01-24 11:24:23 +0100

    Added two models in DataTimeRecorder foder, DataTimeRecorderStart and DataTimeRecorderStop

---
+++ Solution1/ProjectClock.BussinesLogic/Models/DataTimeRecorder/DataTimeRecorderStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Models
+{
+    public class DataTimeRecorderStart
+    {
+        public DateTime TimeStart { get; set; }       
+        public int ProjectID { get; set; }
+        public int UserID {  get; set; }
+        
+    }
+}
---
+++ Solution1/ProjectClock.BussinesLogic/Models/DataTimeRecorder/DataTimeRecorderStop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Models.DataTimeRecorder
+{
+    public class DataTimeRecorderStop
+    {
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeStop { get; set; }
+        public int ProjectID { get; set; }
+        public int UserID { get; set; }
+
+
+    }
+}
