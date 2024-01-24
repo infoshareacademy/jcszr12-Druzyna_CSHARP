600430ab9d318160fd41b4ee21153de3ff692ec4
UndergroundRobotics <robert.nizio@gmail>
2024-01-24 11:34:10 +0100

    Added Demo for WorkingTimeRecorder in Project.Clock.UJ -> Program.cs

--- Solution1/ProjectClock.UI/Program.cs
+++ Solution1/ProjectClock.UI/Program.cs
@@ -4,6 +4,7 @@ using ProjectClock.BusinessLogic.Models;
 using ProjectClock.BusinessLogic.Services;
 using ProjectClock.BusinessLogic.Services.UserServices;
 using ProjectClock.UI.Menu;
+using ProjectClock.BusinessLogic.Services.WorkingTimeRecorder;
 
 
 namespace ProjectClock.UI
@@ -12,7 +13,29 @@ namespace ProjectClock.UI
     {
         static void Main(string[] args)
         {
-            MainMenu.RunMenu();
+            
+            //////////////////////////////////////////////////////
+            //              Demo WorkingTimeRecorder            //
+            //////////////////////////////////////////////////////
+            
+            // WorkingTimeRecorder.WriteToDatabaseSimulationData();
+
+            WorkingTimeRecorder.StartWork(4, 3);
+
+            //WorkingTimeRecorder.StopWork(4, 3);
+
+            //WorkingTimeRecorder.ViewProjectsInProgress();
+
+            //WorkingTimeRecorder.ViewClosedProjects();
+
+            //WorkingTimeRecorder.TotalTimeForProjectID(3);
+
+            //WorkingTimeRecorder.TotalTimeForUserID(5);
+
+            //WorkingTimeRecorder.ClearDatabase();
+
+
+            //MainMenu.RunMenu();
             
         }         
 
