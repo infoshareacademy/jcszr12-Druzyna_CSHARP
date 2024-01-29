19f12d6cc5b8480a07d638e6e02a1bd8661d0267
UndergroundRobotics <robert.nizio@gmail>
2024-01-29 19:44:30 +0100

    Added method: public static TimeSpan TotalTimeForProjectID(int projectId)

--- Solution1/ProjectClock.BussinesLogic/Services/Statistics/Statistics.cs
+++ Solution1/ProjectClock.BussinesLogic/Services/Statistics/Statistics.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using ProjectClock.BusinessLogic.Models.DataTimeRecorder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,61 @@ using System.Threading.Tasks;
 
 namespace ProjectClock.BusinessLogic.Services.Statistics
 {
-    internal class Statistics
+    public class Statistics
     {
+
+
+        public static TimeSpan TotalTimeForProjectID(int projectId)
+        {
+            var dataFromDatabase = GetDataStopTimeFromDatabase();
+            var dataProjectId = dataFromDatabase.Where(p => p.ProjectID == projectId);
+            List<TimeSpan> timeSpans = [];
+            foreach (var result in dataProjectId)
+            {
+                TimeSpan timeSpan = result.TimeStop.Subtract(result.TimeStart);
+                timeSpans.Add(timeSpan);
+            }
+            long timeSpanTics = timeSpans.Sum(p => p.Ticks);
+            TimeSpan timeSpanRaw = new TimeSpan(timeSpanTics);
+            return timeSpanRaw;
+        }
+
+        public static TimeSpan DemoTotalTimeForProjectID(int projectId)
+        {
+            var dataFromDatabase = GetDataStopTimeFromDatabase();
+            var dataProjectId = dataFromDatabase.Where(p => p.ProjectID == projectId);
+            List<TimeSpan> timeSpans = [];
+            Console.WriteLine($"\n\n * Data for projects about ID: {projectId} \n");
+            Console.WriteLine("   * Time for users working on the project about: \n");
+            foreach (var result in dataProjectId)
+            {
+                TimeSpan timeSpan = result.TimeStop.Subtract(result.TimeStart);
+                Console.WriteLine($"     * ID: {result.UserID} is {timeSpan}");
+                timeSpans.Add(timeSpan);
+            }
+            long timeSpanTics = timeSpans.Sum(p => p.Ticks);
+            TimeSpan timeSpanRaw = new TimeSpan(timeSpanTics);
+            Console.WriteLine($"\n\n  * Total time for the projects about ID {projectId} is: {timeSpanRaw.Days}D {timeSpanRaw.Hours}H {timeSpanRaw.Minutes}m {timeSpanRaw.Seconds}s \n\n\n\n");
+
+            return timeSpanRaw;
+        }
+
+        private static List<StopWork> GetDataStopTimeFromDatabase()
+        {
+            var json = File.ReadAllText(GetDirectoryToFileFromDataFolder("recordsOfTimeStop.json"));
+            List<StopWork> dataTimeRecords = JsonConvert.DeserializeObject<List<StopWork>>(json);
+            return dataTimeRecords;
+        }
+
+        private static string GetDirectoryToFileFromDataFolder(string fileName)
+        {
+            var pathOrigin = Directory.GetCurrentDirectory();
+            var index = pathOrigin.IndexOf("Solution1");
+            var pathPart = pathOrigin.Substring(0, index);
+            var path = $@"{pathPart}\Solution1\ProjectClock.BussinesLogic\Data\WorkingTimeRecorder\{fileName}";
+            return path;
+        }
+
+
     }
 }
--- Solution1/ProjectClock.UI/Program.cs
+++ Solution1/ProjectClock.UI/Program.cs
@@ -5,6 +5,8 @@ using ProjectClock.BusinessLogic.Services;
 using ProjectClock.BusinessLogic.Services.UserServices;
 using ProjectClock.UI.Menu;
 using ProjectClock.BusinessLogic.Services.WorkingTimeRecorder;
+using System.Dynamic;
+using ProjectClock.BusinessLogic.Services.Statistics;
 
 
 namespace ProjectClock.UI
@@ -13,7 +15,13 @@ namespace ProjectClock.UI
     {
         static void Main(string[] args)
         {
-            MainMenu.RunMenu();            
+            //MainMenu.RunMenu();
+            //
+
+
+            Console.WriteLine($"\n\n  * Total time for the projects about ID {1} is: {Statistics.TotalTimeForProjectID(1).Days}D {Statistics.TotalTimeForProjectID(1).Hours}H {Statistics.TotalTimeForProjectID(1).Minutes}m {Statistics.TotalTimeForProjectID(1).Seconds}s");
+
+            
         }
 
     }
