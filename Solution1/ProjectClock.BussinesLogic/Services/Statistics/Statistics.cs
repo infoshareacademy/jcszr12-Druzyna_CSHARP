74afb8796accded82fd8b5236be2dedeb5072eef
UndergroundRobotics <robert.nizio@gmail>
2024-01-29 19:18:01 +0100

    Added new class Statistics.cs in Statistics folder

---
+++ Solution1/ProjectClock.BussinesLogic/Services/Statistics/Statistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Services.Statistics
+{
+    internal class Statistics
+    {
+    }
+}
