e871ba3609bea648a6b7bceff63dc2d09fb0533a
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-02-08 18:49:40 +0100

    Organization  manage system  created.

---
+++ Solution1/ProjectClock.MVC/Controllers/RaportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectClock.MVC.Controllers
+{
+    public class RaportController : Controller
+    {
+        public IActionResult Generate()
+        {
+            return View();
+        }
+    }
+}
--- Solution1/ProjectClock.MVC/obj/Debug/net8.0/ProjectClock.MVC.AssemblyInfo.cs
+++ Solution1/ProjectClock.MVC/obj/Debug/net8.0/ProjectClock.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectClock.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50472695336e951912e536a1ba43d7016887978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f584fe68deb2cfe03f6f783f4b2eeb6dc14a5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectClock.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectClock.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
