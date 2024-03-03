adbec4e0ebd76e497f0778d843808a048a97157f
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-03-03 22:28:36 +0100

    Create skeleton for creating accounts

---
+++ Solution1/ProjectClock.BussinesLogic/Dtos/AccountDtos/EditEmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Dtos.AccountDtos
+{
+    public class EditEmailDto
+    {
+        public int UserId { get; set; }
+        public string NewEmail { get; set; }
+    }
+}
---
+++ Solution1/ProjectClock.BussinesLogic/Dtos/AccountDtos/LoginResultDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClock.BusinessLogic.Dtos.AccountDtos
+{
+    public class LoginResultDto
+    {
+
+        public bool LoginWasSuccessful { get; set; }
+
+        public int? UserId { get; set; }
+
+        public AuthenticationProperties? AuthProp { get; set; }
+
+        public ClaimsIdentity? ClaimsIdentity { get; set; }
+    }
+}
