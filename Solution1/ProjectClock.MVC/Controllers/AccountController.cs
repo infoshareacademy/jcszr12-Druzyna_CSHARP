24de74f3ea82bf7aa99c7680277ee4f12509997c
TomaszZukowski <tomaszzukowskibp@gmail.com>
2024-03-16 10:58:45 +0100

    Add comunication with user and create skeleton for apply changes in account

--- Solution1/ProjectClock.MVC/Controllers/AccountController.cs
+++ Solution1/ProjectClock.MVC/Controllers/AccountController.cs
@@ -68,6 +68,7 @@ namespace ProjectClock.MVC.Controllers
 
             var resultDto = await _accountService.RegisterAccount(dto);
             
+
             if (resultDto.RegistrationFailed)
             {
                 return View(dto);
